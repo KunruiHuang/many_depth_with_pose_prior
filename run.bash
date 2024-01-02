a2ca6f7e1d38e0cac6bc33e228016edf34a32283
huangkunrui <huangkunrui@meituan.com>
2024-01-02 12:02:18 +0800

    initial commit

---
+++ run.bash
@@ -0,0 +1,5 @@
+python -m manydepth.test_simple \
+    --target_image_path /home/hkr/Dataset/iphone12_collect_data/2023_12_29_16_28_30/rgb/frame_0308.png \
+    --source_image_path /home/hkr/Dataset/iphone12_collect_data/2023_12_29_16_28_30/rgb/frame_0309.png \
+    --intrinsics_json_path assets/test_sequence_intrinsics.json \
+    --model_path checkpoints/KITTI_HR/
